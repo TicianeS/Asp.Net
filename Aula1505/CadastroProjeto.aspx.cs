99912c129d8a87686a6ce915416616d632dff07a
ti_ti <ti_ti@TICIANE>
2017-05-15 11:40:06 -0300

    Criar forms

---
+++ Aula1505/CadastroProjeto.aspx.cs
@@ -0,0 +1,38 @@
+﻿using Aula1505.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula1505
+{
+    public partial class CadastroProjeto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void LimparCampos()
+        {
+            txtNome.Text = string.Empty;
+            chkAtivo.Checked = false;
+        }
+
+        protected void btnCancelar_Click1(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Projeto prj = new Projeto();
+            prj.Nome = txtNome.Text;
+            prj.Descricao = txtDescricao.Text;
+            prj.Ativo = chkAtivo.Checked;
+        }
+    }
+}
