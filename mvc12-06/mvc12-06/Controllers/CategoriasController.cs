24e0f1389e2927048ca0e0f951b5300c405e9b13
ti_ti <ti_ti@TICIANE>
2017-06-12 10:50:33 -0300

    Tela de cadastro orientada ao objeto Categoria

--- mvc12-06/mvc12-06/Controllers/CategoriasController.cs
+++ mvc12-06/mvc12-06/Controllers/CategoriasController.cs
@@ -3,23 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mvc1206.Models;
 
-namespace mvc12_06.Controllers
+namespace Mvc1206.Controllers
 {
     public class CategoriasController : Controller
     {
         // GET: Categorias
         public ActionResult Index()
         {
-            List<string> categorias = new List<string>();
+            List<Categoria> categorias = new List<Categoria>();
 
+            categorias.Add(new Categoria() { Nome = "Carros" });
+            categorias.Add(new Categoria() { Nome = "Motos" });
+            categorias.Add(new Categoria() { Nome = "Aviões" });
+
+
+            /* 
+             List<string> categorias = new List<string>();
             categorias.Add("Carros");
-            categorias.Add("Motos");
-            categorias.Add("Aviões");
+             categorias.Add("Motos");
+             categorias.Add("Aviões");*/
 
             ViewBag.ListaCategorias = categorias;
 
+            return View(categorias);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            return View(categoria);
         }
     }
 }
