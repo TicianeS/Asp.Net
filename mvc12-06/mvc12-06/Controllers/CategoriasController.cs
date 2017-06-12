ffcc37a877cfd73724dadf37406263502df59579
ti_ti <ti_ti@TICIANE>
2017-06-12 09:25:50 -0300

    Criando controller e view de categorias

---
+++ mvc12-06/mvc12-06/Controllers/CategoriasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc12_06.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<string> categorias = new List<string>();
+
+            categorias.Add("Carros");
+            categorias.Add("Motos");
+            categorias.Add("Aviões");
+
+            ViewBag.ListaCategorias = categorias;
+
+            return View();
+        }
+    }
+}
