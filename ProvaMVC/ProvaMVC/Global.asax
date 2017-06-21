81b6c84dadae65c31d276dc3593fda2627cbeec8
ti_ti <ti_ti@TICIANE>
2017-06-21 09:56:12 -0300

    Criação das models

---
+++ ProvaMVC/ProvaMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProvaMVC.MvcApplication" Language="C#" %>
---
+++ ProvaMVC/ProvaMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProvaMVC.Startup))]
+namespace ProvaMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
