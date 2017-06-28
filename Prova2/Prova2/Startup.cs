b083af71bf756bf020a3fab5beed37487f2620ae
ti_ti <ti_ti@TICIANE>
2017-06-28 10:49:41 -0300

    criei library modelos

---
+++ Prova2/Prova2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prova2.Startup))]
+namespace Prova2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
